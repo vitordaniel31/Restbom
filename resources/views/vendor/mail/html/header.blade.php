0b3d08c9fc5603af44cd74bb2ebb7d499cf49b85
Vitor Medeiros <vitordaniel602@gmail.com>
2021-03-24 12:55:44 -0300

    Personalizando email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="https://restbom.herokuapp.com/images/logo.png" class="logo" alt="Restbom Logo">
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
